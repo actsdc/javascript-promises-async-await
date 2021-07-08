beca267a64b94dd657f040651086cda66ab54476
Stuart Connall <sconnall2@andovercos.com>
2021-07-08 16:05:28 -0400

    Completed module

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ export function fetchMovies() {
   const resolveFunction = () => movies;
   return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+const moviePromise = fetchMovies();
+moviePromise.then((results) => {
+  console.log(results);
+});
