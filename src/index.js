758e61261349ed9578b85c5fc9e20eb48eb578b4
Stuart Connall <sconnall2@andovercos.com>
2021-07-08 17:07:16 -0400

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
+import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
+const movies = require("./data/movies.json");
 
 const getBooksAndMovies = () => {
   return Promise.all([fetchBooks(), fetchMovies()])
     .then(([books, movies]) => ({
-      books, movies
+      books,
+      movies
     }))
-    .catch(error => console.log('Error fething books and movies', error));
+    .catch(error => console.log("Error fething books and movies", error));
 };
 
 const getBooksAndMoviesPromise = getBooksAndMovies();
 getBooksAndMoviesPromise.then(results => {
-  console.log('getBooksAndMoviesPromise', results);
+  console.log("getBooksAndMoviesPromise", results);
 });
 
 function getBooksOrMovies() {
   return Promise.race([fetchBooks(), fetchMovies()])
     .then(results => results)
     .catch(error => {
-      console.log('Error waiting for the promise race', error);
+      console.log("Error waiting for the promise race", error);
     });
 }
 
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {
-  console.log('getBooksOrMoviesPromise', results);
+  console.log("getBooksOrMoviesPromise", results);
 });
