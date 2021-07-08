53ad3ccdd6839e1ebe8d2b7563646cc79ca3a38d
Stuart Connall <sconnall2@andovercos.com>
2021-07-08 19:08:39 -0400

    Completed module

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ async function getBooksAndMoviesAsync() {
 }
 
 async function getBooksOrMoviesAsync() {
-  const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+  const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
   return values;
 }
 
